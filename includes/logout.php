21f83406c66ecb387e8f52b6bc910ec66b307fca
Leafy2001 <51094578+Leafy2001@users.noreply.github.com>
2021-01-13 00:58:34 +0530

    First

---
+++ includes/logout.php
@@ -0,0 +1,14 @@
+<?php ob_start(); session_start(); include "db.php";
+
+if(isset($_SESSION['user_id'])){
+    $_SESSION['user_id'] = null;
+    $_SESSION['username'] = null;
+    $_SESSION['user_firstname'] = null;
+    $_SESSION['user_lastname'] = null;
+    $_SESSION['user_email'] = null;
+    $_SESSION['user_image'] = null;
+    $_SESSION['user_role'] = null;
+    
+}
+    header("Location: ../index.php");
+?>
