f800bfcc5554f3c283178b043003f338eaa82504
Leafy2001 <51094578+Leafy2001@users.noreply.github.com>
2021-01-14 22:28:10 +0530

    s

--- includes/db.php
+++ includes/db.php
@@ -1,16 +1,16 @@
 <?php
 
 //DEVELOPMENT
-define('DB_USER', 'root');
-define('DB_HOST', 'localhost');
-define('DB_PASS', '');
-define('DB_NAME', 'cms');
+// define('DB_USER', 'root');
+// define('DB_HOST', 'localhost');
+// define('DB_PASS', '');
+// define('DB_NAME', 'cms');
 
 //PRODUCTION
-// define('DB_USER', '2kWWEPFoqD');
-// define('DB_HOST', 'remotemysql.com');
-// define('DB_PASS', 'yVWEqsY8Z5');
-// define('DB_NAME', '2kWWEPFoqD');
+define('DB_USER', '2kWWEPFoqD');
+define('DB_HOST', 'remotemysql.com');
+define('DB_PASS', 'yVWEqsY8Z5');
+define('DB_NAME', '2kWWEPFoqD');
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if($connection == false){
